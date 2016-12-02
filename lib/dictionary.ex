a5ef2892646eb95db40aa47604c0855dc6c6b3c0
silesky <seth.silesky@gmail.com>
2016-12-02 16:24:39 -0600

    add terms.json

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,13 +1,27 @@
+
+
+
 defmodule Dictionary do
+
   # mix escript.build
   # watch -n 1 ./dictionary 
   def main(args) do
+    # terms = File.read!("terms.json");
+    IO.inspect parse_terms
     IO.puts "main method!!."
     args 
     |> parse_args 
     |> lookup 
   end
 
+
+  def parse_terms() do
+   { :ok, stuff } = "./lib/terms.json"
+   |> File.read!
+   |> JSON.decode 
+   stuff
+  #returns the last value
+  end
   defp parse_args(args) do
     {_, _, [{_, word}]} = OptionParser.parse(args);
     IO.inspect word
@@ -15,12 +29,12 @@ defmodule Dictionary do
   end
     def lookup(word) do
       case word do
-      "atom" -> IO.puts "atoms - constants with itself as value."
+      "atom" -> IO.puts "atoms - constants! with itself as value."
       "char list" -> IO.puts "list of the 'code points' of each characters, rather than the chars themselves"
       _->  IO.puts "nothing found"
     end
   end
-  def transformer([]) do
+  def transformer([]) do 
     IO.puts "no arguments given"
   end
 end
